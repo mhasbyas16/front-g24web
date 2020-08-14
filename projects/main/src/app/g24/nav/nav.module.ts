08bf84a2d51673baee5a9c89b3109085d772dcca
mhasbyas16 <hasbyas16@gmail.com>
2020-08-14 15:59:42 +0700

    commit

--- projects/main/src/app/g24/nav/nav.module.ts
+++ projects/main/src/app/g24/nav/nav.module.ts
@@ -6,25 +6,16 @@ import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
-import { ModalComponent } from './modal/modal.component';
 import { CartComponent } from './modal/cart/cart.component';
 import { CheckoutComponent } from './modal/checkout/checkout.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddNasabahComponent } from './modal/add-nasabah/add-nasabah.component';
 import { SearchNasabahComponent } from './modal/search-nasabah/search-nasabah.component';
-<<<<<<< HEAD
-import { PilihPembayaranComponent } from './modal/pilih-pembayaran/pilih-pembayaran.component';
-
-
-@NgModule({
-  declarations: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, ModalComponent, CartComponent, CheckoutComponent, AddNasabahComponent, SearchNasabahComponent, PilihPembayaranComponent],
-=======
 import { ModalOutletComponent } from './modal-outlet/modal-outlet.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, ModalComponent, CartComponent, CheckoutComponent, AddNasabahComponent, SearchNasabahComponent, ModalOutletComponent],
->>>>>>> 2a44819d211ca624e820f5cfc6d20c0149addc72
+  declarations: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, CartComponent, CheckoutComponent, AddNasabahComponent, SearchNasabahComponent, ModalOutletComponent],
   imports: [
     CommonModule,
     RouterModule,
