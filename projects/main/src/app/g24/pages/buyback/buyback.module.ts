f148bbe13d5dd8874979b79c03ec2274cf61fd7a
habibitaqwim <thetaqwim@gmail.com>
2020-08-27 14:46:04 +0700

    08272020_11_habibi

---
+++ projects/main/src/app/g24/pages/buyback/buyback.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BuybackRoutingModule } from './buyback-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BuybackRoutingModule
+  ]
+})
+export class BuybackModule { }
