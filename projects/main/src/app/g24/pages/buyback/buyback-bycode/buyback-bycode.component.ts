01bacb526156f5f8625a2eac91ede2cfb5d4255a
habibitaqwim <thetaqwim@gmail.com>
2020-08-30 22:24:25 +0700

    08302020_01_habibi

--- projects/main/src/app/g24/pages/buyback/buyback-bycode/buyback-bycode.component.ts
+++ projects/main/src/app/g24/pages/buyback/buyback-bycode/buyback-bycode.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { EMenuID } from '../../../lib/enums/emenu-id.enum';
 import { DContent } from '../../../decorators/content/pages';
 import { BuybackModule } from '../buyback.module';
-import { FormGroup, FormControl,ReactiveFormsModule  } from '@angular/forms';
+import { FormGroup, FormControl,ReactiveFormsModule, Validators  } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 // services
 import { TransactionService } from '../../../services/transaction/transaction.service';
+
+import { SessionService } from 'projects/platform/src/app/core-services/session.service';
+import { TanggalService } from '../../../lib/helper/tanggal.service';
+
 
 
 @Component({
@@ -20,12 +24,25 @@ import { TransactionService } from '../../../services/transaction/transaction.se
 export class BuybackBycodeComponent implements OnInit {
 
 
-  // badanUsaha: FormGroup = null; 
+  //Form Search
   searchTrans: FormGroup = null;
- 
+  
+  //id response
+  detailTransaction : any;
+  totalDetail : any;
 
+  //tanggal
+  tanggalTerbilang : any;
 
-  constructor() { }
+  loadingDg: boolean = false;
+  placeholderDatagrid = "Silahkan Cari Transaksi Berdasarkan Parameter";
+
+  constructor(
+    private toastrService:ToastrService,
+    private transactionService: TransactionService,
+    // private sessionService: SessionService,
+    private tanggalService:TanggalService,
+  ) { }
 
   ngOnInit(): void {
     this.isiForm();
@@ -38,9 +55,38 @@ export class BuybackBycodeComponent implements OnInit {
     });
   }
 
-  searchTransaction(val){
-   
+  searchTransaction(){
+
     
+
+    this.loadingDg = true;
+    this.totalDetail = 0;
+    if (!this.searchTrans.valid) {
+      this.toastrService.error("Harap Input Id", "Buyback");
+      this.loadingDg = false;
+      return;
+    }
+    let data = this.searchTrans.getRawValue();
+     
+    this.transactionService.get("?idTransaction="+data.text).subscribe((response:any)=>{
+      if (response == false) {
+        this.toastrService.error("Transaksi Tidak Ditemukan", "Buyback");
+        this.loadingDg = false;
+        return
+      }
+      this.detailTransaction = response
+      this.totalDetail = 1
+
+      let tgl =this.detailTransaction.makerDate;
+      let tglSplit = tgl.split("/");
+      let bulan = Number(tglSplit["0"]);
+      let hari = tglSplit["1"];
+      let tahun = tglSplit["2"];
+      let bulanTerbilang = this.tanggalService.bulanGenerate(bulan);
+
+      this.tanggalTerbilang = hari+" "+bulanTerbilang+" "+tahun
+      this.loadingDg = false;
+    })
   
   }
 }
--- projects/main/src/app/g24/pages/buyback/buyback.module.ts
+++ projects/main/src/app/g24/pages/buyback/buyback.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClarityModule } from '@clr/angular';
+import { ToastrModule } from 'ngx-toastr';
 
 import { BuybackRoutingModule } from './buyback-routing.module';
 import { BuybackBycodeComponent } from './buyback-bycode/buyback-bycode.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavModule } from '../../nav/nav.module';
+
 
 
 @NgModule({
@@ -14,7 +17,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BuybackRoutingModule,
     ClarityModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule,
+    NavModule
   ]
 })
 export class BuybackModule { }
