6dbd8dca703e2662289595518dcdb0b9eb584822
mhasbyas16 <hasbyas16@gmail.com>
2020-09-28 16:07:38 +0700

    prm filter

--- projects/main/src/app/g24/pages/buyback/parameter-buyback/parameter-buyback.component.ts
+++ projects/main/src/app/g24/pages/buyback/parameter-buyback/parameter-buyback.component.ts
@@ -9,7 +9,7 @@ import { ContentPage } from '../../../lib/helper/content-page';
 // session
 import { SessionService } from 'projects/platform/src/app/core-services/session.service';
 import { BuybackParameterService } from '../../../services/buyback/buyback-parameter.service';
-
+import { SplitDateServiceService } from '../../../services/split-date-service.service';
 
 @Component({
   selector: 'app-parameter-buyback',
@@ -25,10 +25,12 @@ export class ParameterBuybackComponent implements OnInit {
   confirmation:boolean = false;
 
   formAdd: FormGroup = null;
+  search: FormGroup = null;
 
   nikUser:any= null;
   modalTitle:any;
   _id:any;
+  vertical = '';
 
   dataSearch=[];
 
@@ -36,7 +38,8 @@ export class ParameterBuybackComponent implements OnInit {
     private sessionService:SessionService,
     private datePipe:DatePipe,
     private buybackParameterService:BuybackParameterService,
-    private toastrService:ToastrService
+    private toastrService:ToastrService,
+    private splitDateServiceService:SplitDateServiceService
   ) { }
 
   ngOnInit(): void {
@@ -44,6 +47,7 @@ export class ParameterBuybackComponent implements OnInit {
     this.nikUser = this.sessionService.getUser();
     this.nikUser = {"_hash":btoa(JSON.stringify(this.nikUser)),"role":this.nikUser.role.name} ;
     this.form();
+    this.formSearch();
     this.dataList();
   }
 
@@ -61,6 +65,43 @@ export class ParameterBuybackComponent implements OnInit {
     });
   }
 
+  filterTransaction(){
+    if (!this.search.valid) {
+      this.toastrService.error("Form Not Valid");
+        this.dataSearch = [];
+        return;
+    }
+    let data = this.search.getRawValue();
+    console.debug(data);
+
+    let from = this.splitDateServiceService.split(data.from);
+    let to = this.splitDateServiceService.split(data.to);
+    let flag:any;
+    if (data.options == "all" || data.options == "") {
+     flag = ""; 
+    }else {
+      flag = "&flag="+data.options;
+    
+    }
+    
+
+    this.buybackParameterService.list("?_hash=1&_sortby=flag:1&_between=makerDate&_start=" + from + "&_end=" + to + flag).subscribe((response:any)=>{
+      if (response == false || response["length"] == 0) {
+        this.toastrService.error("Data Not Found");
+        this.dataSearch = [];
+        return;
+      }
+      this.dataSearch = response;
+      this.toastrService.success("Load Data "+this.dataSearch.length);
+    });
+  }
+  formSearch(){
+    this.search = new FormGroup ({
+      from: new FormControl (this.datePipe.transform(Date.now(),'MM/dd/yyyy')),
+      to: new FormControl (this.datePipe.transform(Date.now(),'MM/dd/yyyy')),
+      options: new FormControl ("", Validators.required)
+    });
+  }
   form(){
     this.formAdd = new FormGroup ({
       maker: new FormControl (this.nikUser._hash),
